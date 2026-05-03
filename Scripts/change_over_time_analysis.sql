4a9de4945e94622f99497f80c73865daba776ddd
Divya Debashrita Basantray <divyadebashrita1307@gmail.com>
2026-05-03 13:38:52 +0530

    Restore comments and SQL queries for analysis

--- Scripts/change_over_time_analysis.sql
+++ Scripts/change_over_time_analysis.sql
@@ -28,6 +28,7 @@ GROUP BY YEAR(order_date), MONTH(order_date)
 ORDER BY YEAR(order_date), MONTH(order_date);
 
 -- DATETRUNC()
+
 SELECT
     DATETRUNC(month, order_date) AS order_date,
     SUM(sales_amount) AS total_sales,
